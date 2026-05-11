e1b3b63c5da057c24809501d722835e7bafb4fa0
Billy <billyjrsantos@outloook.com>
2026-05-11 00:27:39 -0300

    Adicionando DataValidator para validar se é número ou não

---
+++ DataValidator.cpp
@@ -0,0 +1,30 @@
+#include "DataValidator.h"
+
+namespace CategorizacaoParalela {
+    bool DataValidator::ehNumero(const std::string& s) {
+        if (s.empty()) return false;
+        size_t tam_string = s.length();
+
+        int pontos = 0;
+        int inicio = 0;
+
+        if (s[0] == '-') {
+            if (tam_string == 1) return false;
+            inicio = 1;
+        }
+
+        for (int i = inicio; i < tam_string; i++) {
+            if (s[i] == '.') {
+                pontos++;
+                if (pontos > 1) return false;
+            }
+            else if (s[i] < '0' || s[i] > '9') {
+                return false;
+            }
+        }
+
+        if (s == ".") return false;
+
+        return true;
+    }
+}
---
+++ DataValidator.h
@@ -0,0 +1,15 @@
+#ifndef CATEGORIZACAOPARALELA_DATAVALIDATOR_H
+#define CATEGORIZACAOPARALELA_DATAVALIDATOR_H
+
+#include <string>
+
+namespace CategorizacaoParalela {
+
+    class DataValidator {
+    public:
+        static bool ehNumero(const std::string& s);
+    };
+
+}
+
+#endif //CATEGORIZACAOPARALELA_DATAVALIDATOR_H
