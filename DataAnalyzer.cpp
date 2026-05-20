0f07d8a4aa3633c397d680199527d07f91d34b74
Billy <billyjrsantos@outloook.com>
2026-05-19 22:01:16 -0300

    Adicionando esqueleto para funções básicas de estatística descritiva

--- DataAnalyzer.cpp
+++ DataAnalyzer.cpp
@@ -1,1 +1,24 @@
 #include "DataAnalyzer.hpp"
+
+namespace CategorizacaoParalela {
+    float DataAnalyzer::mean(const std::vector<float>) {
+        return 0.0;
+    }
+
+    float DataAnalyzer::median(const std::vector<float>) {
+        return 0.0;
+    }
+
+    float DataAnalyzer::variance(const std::vector<float>) {
+        return 0.0;
+    }
+
+    float DataAnalyzer::std_deviation(const std::vector<float>) {
+        return 0.0;
+    }
+
+    float DataAnalyzer::iqr(const std::vector<float>) {
+        return 0.0;
+    }
+
+}
--- DataAnalyzer.hpp
+++ DataAnalyzer.hpp
@@ -1,4 +1,21 @@
 #ifndef CATEGORIZACAOPARALELA_DATAANALYZER_HPP
 #define CATEGORIZACAOPARALELA_DATAANALYZER_HPP
 
+#include <vector>
+
+namespace CategorizacaoParalela {
+    class DataAnalyzer {
+    public:
+        static float mean(const std::vector<float>);
+
+        static float median(const std::vector<float>);
+
+        static float variance(const std::vector<float>);
+
+        static float std_deviation(const std::vector<float>);
+
+        static float iqr(const std::vector<float>);
+    };
+}
+
 #endif //CATEGORIZACAOPARALELA_DATAANALYZER_HPP
