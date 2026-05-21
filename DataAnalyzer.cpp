fb04d125dceedb3701e58a5a08d54d30c7462635
Billy <billyjrsantos@outloook.com>
2026-05-21 00:57:35 -0300

    Adicionando método de mediana

--- DataAnalyzer.cpp
+++ DataAnalyzer.cpp
@@ -16,7 +16,18 @@ namespace CategorizacaoParalela {
     }
 
     float DataAnalyzer::median(const std::vector<float> vector) {
-        return 0.0;
+        if (vector.empty()) {
+            return 0.0f;
+        }
+
+        const std::size_t n = vector.size();
+        const std::size_t mid = n / 2;
+
+        if (n % 2 != 0) {
+            return vector[mid];
+        }
+
+        return (vector[mid - 1] + vector[mid]) * 0.5f;
     }
 
     float DataAnalyzer::variance(const std::vector<float> vector) {
