0f157d148dc06a6166c73c21b8384ab6cfdfa8a2
Billy <billyjrsantos@outloook.com>
2026-05-24 01:01:10 -0300

    refactor: Melhorando código do analyser

--- DataAnalyzer.cpp
+++ DataAnalyzer.cpp
@@ -1,38 +1,38 @@
 #include "DataAnalyzer.hpp"
 
 namespace CategorizacaoParalela {
-    float DataAnalyzer::mean(const std::vector<float> vector) {
-        int size = vector.size();
+    float DataAnalyzer::mean(const std::vector<float>& vector) {
+        int size = (int)vector.size();
         
-        float sum = 0.0, mean = 0.0;
+        float sum = 0.0f, mean = 0.0f;
 
         for (int i = 0; i < size; ++i) {
             sum += vector[i];
         }
 
-        mean = sum / size;
+        mean = sum / (float)size;
 
         return mean;
     }
 
-    float DataAnalyzer::median(const std::vector<float> vector) {
+    float DataAnalyzer::median(const std::vector<float>& vector) {
         if (vector.empty()) {
             return 0.0f;
         }
 
-        const std::size_t n = vector.size();
-        const std::size_t mid = n / 2;
+        const int size = (int)vector.size();
+        const int mid = size / 2;
 
-        if (n % 2 != 0) {
+        if (size % 2 != 0) {
             return vector[mid];
         }
 
         return (vector[mid - 1] + vector[mid]) * 0.5f;
     }
 
-    float DataAnalyzer::variance(const std::vector<float> vector) {
-        float variance = 0.0;
-        int vector_size = vector.size();
+    float DataAnalyzer::variance(const std::vector<float>& vector) {
+        float variance = 0.0f;
+        int vector_size = (int)vector.size();
 
         if (vector.empty()) {
             return variance;
@@ -48,17 +48,17 @@ namespace CategorizacaoParalela {
             sum_sqs += dif * dif;
         }
 
-        variance = sum_sqs / vector_size;
+        variance = sum_sqs / (float)vector_size;
 
         return variance;
     }
 
-    float DataAnalyzer::std_deviation(const std::vector<float> vector) {
-        return 0.0;
+    float DataAnalyzer::std_deviation(const std::vector<float>& vector) {
+        return 0.0f;
     }
 
-    float DataAnalyzer::iqr(const std::vector<float> vector) {
-        return 0.0;
+    float DataAnalyzer::iqr(const std::vector<float>& vector) {
+        return 0.0f;
     }
 
 }
